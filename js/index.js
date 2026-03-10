2db2cc01ec8f805aad059c8574e9af607af794eb
Fatimae13 <212909285+Fatimae13@users.noreply.github.com>
2026-03-09 21:58:23 -0500

    edits

--- js/index.js
+++ js/index.js
@@ -70,3 +70,44 @@ messageForm.addEventListener("submit", function(event){
 
     messageForm.reset();
 });
+
+///---fetch---
+
+fetch("https://api.github.com/users/Fatimae13/repos")
+    .then((response) => {
+        if(!response.ok){
+            throw new Error("Failed to fetch data from GitHub.Please try again later");
+        }
+
+        return response.json();
+
+    })
+    .then((repositories) => {
+        //repositories= json.parse(this.respositories);
+        console.log("Repositories:",repositories);
+        const projectSection = document.getElementById("Projects");
+        const projectList = projectSection.querySelector("ul");
+        projectList.innerHTML = "";
+
+        for (let i=0; i < repositories.length; i++) {
+            const project = document.createElement("li");
+            const link = document.createElement("a");
+            link.href = repositories[i].html_url;
+            link.textContent = repositories[i].name;
+            project.appendChild(link);
+            projectList.appendChild(project);
+
+        }
+        })
+
+    .catch((error) => {
+        console.error("Error fetching repositories:",error);
+
+        const projectSection = document.getElementById("Projects");
+        const errorMessage = document.createElement("p");
+        errorMessage.innerHTML = 'Unable to load projects. Please try again later';
+        projectSection.appendChild(errorMessage);
+
+    });
+
+    
